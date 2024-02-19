4451fdc87d733bc2fd20e3c4d8feb60595dc2d25
Edwin Mackenzie-Owen <edwin.mowen@gmail.com>
2024-02-20 00:32:44 +0100

    refactor: even more dry

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
       "x86_64-linux"
     ];
 
-    # Generates
+    # Generate
     # {
     #   aarch64-linux = <pkgs for aarch64-linux>;
     #   x86_64-linux = <pkgs for x86_64-linux>;
@@ -27,8 +27,33 @@
     # }
     pkgsEachSystem = genAttrs supportedSystems (system: import nixpkgs {inherit system;});
 
-    # Given a devShellFunc (pkgs: pkg: <devShell>),
-    # generates devShells for all derivations in nixpkgs
+    # Override stdenv in mkShell
+    # Default is GCC on linux, clang on darwin
+    stdenvOverrides = {
+      default = pkgs: {};
+      clang = pkgs: {stdenv = pkgs.clangStdenv;};
+      gcc = pkgs: {stdenv = pkgs.gccStdenv;};
+    };
+
+    # Apply a devShellFunc (pkgs: pkg: <devShell>)
+    # that generates a devShell for package pkg in package set pkgs
+    # to every package in pkgs
+    # In other words, convert set of packages to a set of devShells
+    mkDevShellsForPkgs = {
+      pkgs,
+      devShellFunc,
+    }:
+      mapAttrsRecursiveCond
+      # Derivations are also attributeSets,
+      # but they have an attribute "type" = "derivation";
+      ({type ? "unknown", ...}: type != "derivation")
+      (
+        _: pkg:
+          devShellFunc pkgs pkg
+      )
+      pkgs;
+
+    # Generate devShell set for each system
     # {
     #   aarch64-linux = {
     #     foo = "<devShell for foo>";
@@ -37,86 +62,101 @@
     #   };
     #   x86_64-linux = ...
     # };
-    mkDevShells = mkDevShellFunc:
+    mkDevShellsEachSystem = devShellFunc:
       genAttrs supportedSystems (
         system: let
           pkgs = pkgsEachSystem."${system}";
         in
-          mapAttrsRecursiveCond
-          # Derivations are also attributeSets,
-          # but they have an attribute "type" = "derivation";
-          ({type ? "unknown", ...}: type != "derivation")
-          (
-            _: pkg:
-              mkDevShellFunc pkgs pkg
-          )
-          pkgs
+          mkDevShellsForPkgs {inherit pkgs devShellFunc;}
       );
 
-    stdenvOverrides = {
-      default = pkgs: {};
-      clang = pkgs: {stdenv = pkgs.clangStdenv;};
-      gcc = pkgs: {stdenv = pkgs.gccStdenv;};
+    # Generate devShells for each stdenvOverride
+    # {
+    #   default = {
+    #     aarch64-linux = {
+    #       fooPackage = "<foo devShell";
+    #       barPackage = "<bar devShell";
+    #     };
+    #     x86_64-linux = {
+    #       ...
+    #     };
+    #   };
+    #   clang = {
+    #     ...
+    #   };
+    # }
+    mkDevShellsEachStdenvOverride = devShellFunc:
+      mapAttrs (
+        _: stdenvOverride:
+          mkDevShellsEachSystem (pkgs: pkg:
+            devShellFunc {
+              inherit pkgs pkg;
+              mkShellOverride = stdenvOverride pkgs;
+            })
+      )
+      stdenvOverrides;
+
+    devShellFunctions = rec {
+      cDevShells = {
+        pkgs,
+        pkg,
+        mkShellOverride ? {},
+        extraPackages ? [],
+        shellHookPre ? "",
+      }:
+        pkgs.mkShell.override mkShellOverride {
+          packages = [pkgs.bear pkgs.clang-tools pkgs.ccls pkgs.zsh] ++ extraPackages;
+          inputsFrom = [pkg];
+
+          hardeningDisable = ["fortify"];
+
+          shellHook =
+            shellHookPre
+            + ''
+              exec zsh
+            '';
+        };
+
+      mesonDevShells = {
+        pkgs,
+        pkg,
+        mkShellOverride ? {},
+        extraPackages ? [],
+        shellHookPre ? "",
+      }:
+        cDevShells {
+          inherit pkgs pkg mkShellOverride extraPackages;
+          shellHookPre =
+            shellHookPre
+            + ''
+              ln -s ./build/compile_commands.json
+              meson setup build
+              ninja -C build
+            '';
+        };
     };
-
-    mkCDevShell = {
-      pkgs,
-      pkg,
-      mkShellOverride ? {},
-      extraPackages ? [],
-      shellHookPre ? "",
-    }:
-      pkgs.mkShell.override mkShellOverride {
-        packages = [pkgs.bear pkgs.clang-tools pkgs.ccls pkgs.zsh] ++ extraPackages;
-        inputsFrom = [pkg];
-
-        hardeningDisable = ["fortify"];
-
-        shellHook =
-          shellHookPre
-          + ''
-            exec zsh
-          '';
-      };
-
-    mkMesonDevShell = {
-      pkgs,
-      pkg,
-      mkShellOverride ? {},
-      extraPackages ? [],
-      shellHookPre ? "",
-    }:
-      mkCDevShell {
-        inherit pkgs pkg mkShellOverride extraPackages;
-        shellHookPre =
-          shellHookPre
-          + ''
-            ln -s ./build/compile_commands.json
-            meson setup build
-            ninja -C build
-          '';
-      };
-  in {
-    cdevShells =
-      mapAttrs (
-        _: stdenvOverride:
-          mkDevShells (pkgs: pkg:
-            mkCDevShell {
-              inherit pkgs pkg;
-              mkShellOverride = stdenvOverride pkgs;
-            })
-      )
-      stdenvOverrides;
-
-    mesonDevShells =
-      mapAttrs (
-        _: stdenvOverride:
-          mkDevShells (pkgs: pkg:
-            mkMesonDevShell {
-              inherit pkgs pkg;
-              mkShellOverride = stdenvOverride pkgs;
-            })
-      )
-      stdenvOverrides;
-  };
+  in
+    # Generate devShells for each devShellFunc
+    # {
+    #   fooDevShells = {
+    #     default = {
+    #       aarch64-linux = {
+    #         fooPackage = "<foo devShell";
+    #         barPackage = "<bar devShell";
+    #       };
+    #       x86_64-linux = {
+    #         ...
+    #       };
+    #     };
+    #     clang = {
+    #       ...
+    #     };
+    #   };
+    #   barDevShells = ...
+    # }
+    mapAttrs (
+      _: devShellFunc:
+        mkDevShellsEachStdenvOverride devShellFunc
+    )
+    devShellFunctions;
 }
